82fd49cb9f7a3a2d1f483544348df3892c827382
wKoja <cfwilliam.ferreira@gmail.com>
2022-01-20 15:34:05 -0300

    initial commit

---
+++ js/app.test.js
@@ -0,0 +1,14 @@
+const request = require("supertest");
+const app = require("./app");
+
+describe("Testes API", () => {
+  it("GET /movies --> retorna todos filmes", () => {});
+
+  it("GET /movies/id --> retorna filme por ID", () => {});
+
+  it("POST /movies --> salva um filme no banco de dados", () => {});
+
+  it("PUT /movies/id --> altera um filme no banco de dados", () => {});
+
+  it("DELETE /movies/id --> deleta um filme por id", () => {});
+});
